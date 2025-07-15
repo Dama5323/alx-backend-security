7ebdf3e64c02dd120f94255b2f09b4c69a25451b
Dama5323 <your-deenyashke@gmail.com>
2025-07-15 06:37:23 +0300

    Enhance logging with geolocation data (country, city).

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,6 +1,8 @@
 from django.utils.timezone import now
 from django.http import HttpResponseForbidden
 from .models import RequestLog, BlockedIP
+import requests
+from django.core.cache import cache
 
 class LogIPMiddleware:
     def __init__(self, get_response):
@@ -14,6 +16,19 @@ class LogIPMiddleware:
         # Check if IP is blacklisted
         if BlockedIP.objects.filter(ip_address=ip).exists():
             return HttpResponseForbidden("<h1>403 Forbidden</h1><p>Your IP is blocked.</p>")
+        
+        # Check cache for geolocation info
+        geo_info = cache.get(ip)
+        if not geo_info:
+            try:
+                response = requests.get(f"http://ip-api.com/json/{ip}").json()
+                geo_info = {
+                    "country": response.get("country", ""),
+                    "city": response.get("city", "")
+                }
+                cache.set(ip, geo_info, timeout=60 * 60 * 24)  # cache for 24h
+            except:
+                geo_info = {"country": "", "city": ""}
 
 
         # Log the request
--- ip_tracking/models.py
+++ ip_tracking/models.py
@@ -13,4 +13,10 @@ class BlockedIP(models.Model):
 
     def __str__(self):
         return self.ip_address
-
+    
+class RequestLog(models.Model):
+    ip_address = models.GenericIPAddressField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    path = models.CharField(max_length=2048)
+    country = models.CharField(max_length=100, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
